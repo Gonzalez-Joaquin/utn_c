905155811bf5dae4e83ab9632dbcd65063c3bfec
Joaco <jgonzales@infosis.tech>
2026-02-13 18:36:30 -0300

    ejercicio_5

---
+++ ejercicio_4/index.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define PI 3.14159265358979323846
+
+float areaCirculo(float radio){
+  return PI * radio * radio;
+}
+
+int main(){
+  float radio;
+
+  printf("Ingrese el radio de un circulo: ");
+  scanf("%f", &radio);
+
+  printf("El area es: %f\n", areaCirculo(radio));
+  
+  return 0;
+}
