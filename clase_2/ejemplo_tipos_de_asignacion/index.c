f0a9164e3ea12a8fce69f67aef477500fa4904a0
Joaco <jgonzales@infosis.tech>
2026-02-18 20:18:41 -0300

    fer tipos de datos de asignación

---
+++ clase_2/ejemplo_tipos_de_asignacion/index.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  int numero1 = 2, numero2 = 3;
+  char inicial = 'J';
+  
+  printf("La primer i es: %i, la segunda i es: %i\n", numero1, numero2);
+  // print("La primer i es: numero1, la segunda i es: numero2");
+  printf("La primera i es: %i, la segunda i es: %i\n", numero2, numero1);
+  // print("La primer i es: numero2, la segunda i es: numero1");
+
+  printf("El nombre de Joaquin es: %c, el número es: %i \n" ,numero1, inicial );
+
+  return 0;
+}
