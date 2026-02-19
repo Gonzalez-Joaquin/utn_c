efec938dd36345b6913b5c1217d329166e5d3161
Joaco <jgonzales@infosis.tech>
2026-02-19 08:09:24 -0300

    fix: error en ejercicio 6

--- clase_2/ejercicio_6/index.c
+++ clase_2/ejercicio_6/index.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
   int num1, num2;
 
   printf("Ingrese número 1:");
@@ -10,18 +11,22 @@ int main() {
   scanf("%i", &num2);
 
   // Otra manera de pensar.
-  if(num1 == num2) {
+  if (num1 == num2)
+  {
     printf("Ambos números son iguales\n");
     return 0; // Esto es para que finalice el bloque de código y no siga ejecutando el resto de la función
   }
-  
+
   printf("El orden de forma ascendente es: ");
-  
-  if( num1 > num2 ) {
-    printf("%i, %i \n", num1, num2);
-  } else {
-    printf("%i, %i \n", num2, num1);
+
+  if (num1 > num2)
+  {
+    printf("%d, %d\n", num2, num1);
   }
-  
+  else
+  {
+    printf("%d, %d\n", num1, num2);
+  }
+
   return 0;
 }
